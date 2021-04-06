aa16cc2534042890b53e2e7f4d96566ea30911b7
KeunHee Park <geunii2016@gmail.com>
2021-04-06 18:15:39 +0900

    update mainPage

---
+++ src/main/java/com/megait/soir/repository/CodyRepository.java
@@ -0,0 +1,16 @@
+package com.megait.soir.repository;
+
+import com.megait.soir.domain.Cody;
+import com.megait.soir.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
+
+@Repository
+public interface CodyRepository extends JpaRepository<Cody, Long> {
+
+    public List<Cody> findAllByMember(Member member);
+
+}
